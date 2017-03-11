f545d850810e96a4a469b495673dd742e4e952d9
prasad renukdas <prasad renukdas>
2017-03-11 16:39:48 +0530

    task1 armentum

---
+++ AwesomeProject/Cart.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  Navigator,
+} from 'react-native';
+
+export class Cart extends Component{
+render(){
+return(
+<View>
+
+</View>
+
+
+);
+
+
+
+}
+
+
+
+}
---
+++ AwesomeProject/index.android.js
@@ -0,0 +1,208 @@
+'use strict';
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  TouchableHighlight,
+  Navigator,
+} from 'react-native';
+
+var AwesomeProject = React.createClass({
+
+  renderScene(route, navigator) {
+    if (route.name == 'Main') {
+      return <Main navigator={navigator} {...route.passProps} />
+    }
+    if (route.name == 'Home') {
+      return <Home navigator={navigator} {...route.passProps} />
+    }
+  },
+
+  render() {
+    return (
+      <Navigator
+        style={{ flex: 1 }}
+        initialRoute={{ name: 'Main' }}
+        renderScene={this.renderScene} />
+    )
+  }
+});
+
+var Main = React.createClass({
+  _navigate(name) {
+    this.props.navigator.push({
+      name: 'Home',
+      passProps: {
+        name: name
+      }
+    })
+  },
+  render() {
+    return (
+      <View style={styles.container} >
+
+        <View style={styles.image}>
+          <Image style={styles.imageDim} source={require('./Assets/landing-page.png')}>
+            <Text style={styles.companyTag1} >Food</Text>
+            <Text style={styles.companyTag2} >Panda</Text>
+            <Text style={styles.tagHeading}>WHAT A TWIST. </Text>
+            <Text style={styles.tagContent}>The Panda, the iconic long, slim slick of bread, has{'\n'} traditionally one of the most potent symbols of french{'\n'} culture. </Text>
+          </Image>
+        </View>
+
+
+        <View style={styles.button}  >
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Logo.jpg')} />
+          </TouchableHighlight>
+
+
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')} >
+            <Image source={require('./Assets/Home_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Menu_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Order_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Notifi_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+
+        </View>
+
+
+      </View>
+    )
+  }
+})
+
+var Home = React.createClass({
+  render() {
+    return (
+      <View style={styles.container} >
+
+        <View style={styles.image}>
+
+          
+          <Text >Hello from {this.props.name}</Text>
+          <TouchableHighlight onPress={() => this.props.navigator.pop()}>
+            <Text >GO Back</Text>
+          </TouchableHighlight>
+        </View>
+
+        <View style={styles.button}  >
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Logo.jpg')} />
+          </TouchableHighlight>
+
+
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')} >
+            <Image source={require('./Assets/Home_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Menu_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Order_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+          <TouchableHighlight onPress={() => this._navigate('YOYOYOYOYO')}>
+            <Image source={require('./Assets/Notifi_Btn_nrm.png')} />
+          </TouchableHighlight>
+
+
+        </View>
+      </View>
+    )
+  }
+})
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+
+  },
+  heading: {
+    fontSize: 22,
+    marginBottom: 10
+  },
+  button: {
+    flex: 1,
+    justifyContent: 'space-around',
+
+    alignItems: 'center',
+    flexDirection: 'row',
+
+  },
+  buttonText: {
+    fontSize: 20
+  },
+  image: {
+    flex: 10,
+    flexDirection: 'row',
+    justifyContent: 'center',
+
+  },
+  imageDim: {
+    width: 400,
+    height: 520
+  },
+  companyTag1: {
+    color: 'white',
+    paddingLeft: 35,
+    paddingTop: 175,
+    fontSize: 50,
+    justifyContent: 'center',
+
+
+  },
+  companyTag2: {
+    color: 'white',
+    paddingLeft: 35,
+    justifyContent: 'center',
+    fontSize: 55,
+
+  },
+  tagHeading: {
+    color: 'white',
+    paddingLeft: 35,
+
+    fontSize: 20,
+
+  },
+  tagContent: {
+    color: 'white',
+    paddingLeft: 35,
+
+    fontSize: 14,
+
+  }
+
+});
+
+AppRegistry.registerComponent('AwesomeProject', function () {
+
+  return AwesomeProject;
+});
